af735564900d0e96fb51f2b638c6c5987c52117d
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2017-04-20 08:47:54 +0300

    Theme Switch layout moved to Navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,22 +2,33 @@ import { Component, OnInit } from '@angular/core';
 
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+	selector: 'app-navbar',
+	templateUrl: './navbar.component.html',
+	styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
 
-	showThemes: boolean = false;
+	switchTheme: boolean = false;
 
-  constructor() {}
+	themeData = [
+		{ color: 'black', theme: 't-black', deg: 90 },
+		{ color: 'white', theme: 't-white', deg: 0 },
+		{ color: 'yellow', theme: 't-yellow', deg: 180 },
+		{ color: 'purple', theme: 't-purple', deg: -90 }
+	];
 
-  onShowThemes() {
-  	this.showThemes = !this.showThemes;
-  	console.log(this.showThemes);
-  }
+	constructor() {}
 
-  ngOnInit() {
-  }
+	onSwitchThemes() {
+		this.switchTheme = !this.switchTheme;
+		console.log(this.switchTheme);
+	}
+
+	changeTheme() {
+		
+	}
+
+	ngOnInit() {
+	}
 
 }
