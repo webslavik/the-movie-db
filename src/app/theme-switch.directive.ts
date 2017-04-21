a4c033078057be69de044b3eb8714e9f99ea172e
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2017-04-21 09:40:19 +0300

    optimized method onClick() in theme-switch.directive

--- src/app/theme-switch.directive.ts
+++ src/app/theme-switch.directive.ts
@@ -21,26 +21,37 @@ export class ThemeSwitchDirective implements AfterViewInit {
     const deg = this.elem.dataset.rotate;
     const theme = this.elem.dataset.theme;
 
-    for (let i = 0; i < siblings.length; i++) {
-      siblings[i].classList.remove('is-active');
+
+    if (this.elem.classList.contains('is-active')) {
+
+      setTimeout(() => {
+        this.hideThemeSwitch();
+      }, 150);
+
+    } else {
+
+      for (let i = 0; i < siblings.length; i++) {
+        if (siblings[i].classList.contains('is-active'))
+          siblings[i].classList.remove('is-active');
+      }
+
+      this.elem.classList.add('is-selected');
+
+      parent.style.transform = `rotate(${deg}deg)`;
+      this.body.className = '';
+      this.body.classList.add(theme);
+
+      setTimeout(() => {
+        this.hideThemeSwitch();
+        this.elem.classList.remove('is-selected');
+      }, 700);
+
+      setTimeout(() => {
+        this.elem.classList.add('is-active');
+      }, 900);
     }
-
-    parent.style.transform = `rotate(${deg}deg)`;
-    this.body.className = '';
-    this.body.classList.add(theme);
-
-    this.elem.classList.add('is-selected');
-
-    setTimeout(() => {
-      this.hideThemeSwitch();
-      this.elem.classList.remove('is-selected');
-    }, 700);
-
-    setTimeout(() => {
-      this.elem.classList.add('is-active');
-    }, 900);
-
   }
+
 
   hideThemeSwitch() {
     this.onHide.emit();
@@ -48,7 +59,7 @@ export class ThemeSwitchDirective implements AfterViewInit {
 
   ngAfterViewInit() {
     this.elem = this.el.nativeElement;
-    // this.elem.parentElement.children[1].classList.add('is-selected');
+    this.elem.parentElement.children[1].classList.add('is-active');
   }
 
 }
