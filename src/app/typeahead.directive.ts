393dec24a64b6edf7c24097a589db62c93c7f0b4
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2017-04-23 09:59:38 +0300

    typeahead gets data from TMDb

--- src/app/typeahead.directive.ts
+++ src/app/typeahead.directive.ts
@@ -8,51 +8,27 @@ import 'typeahead.js';
 })
 export class TypeaheadDirective implements AfterViewInit {
 
-	states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-		'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-		'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-		'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-		'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-		'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-		'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-		'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-		'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-	];
-
 	constructor(private el: ElementRef) { }
 
+
 	ngAfterViewInit() {
-		$(this.el.nativeElement).typeahead({
-			hint: true,
-			highlight: true,
-			minLength: 1
-		},
-		{
-			name: 'states',
-			source: this.substringMatcher(this.states)
-		})
-	}
 
-	substringMatcher(strs) {
-		return function findMatches(q, cb) {
-			var matches, substringRegex;
-
-			// an array that will be populated with substring matches
-			matches = [];
-
-			// regex used to determine if a string contains the substring `q`
-			let substrRegex = new RegExp(q, 'i');
-
-			// iterate through the pool of strings and for any string that
-			// contains the substring `q`, add it to the `matches` array
-			$.each(strs, function(i, str) {
-				if (substrRegex.test(str)) {
-					matches.push(str);
+		$(this.el.nativeElement).typeahead(null, {
+			name: 'picture',
+			limit: 10,
+			source: function(query, suncResults, asyncResults) {
+				$.get(`https://api.themoviedb.org/3/search/movie?api_key=48b40155da6e1c749302058b3380da7a&query=${query}`, function(data) {
+					console.log(data.results);
+					asyncResults(data.results);
+				});
+			},
+			templates: {
+				suggestion: function(data) {
+					return `<h2>${data.original_title}</h2>`;
 				}
-			});
+			}
+		});
 
-			cb(matches);
-		};
-	};
 
+	}
 }
