86ebf6c241d4c27420e1573f6bd4b7c0637675c2
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2017-05-14 14:16:03 +0300

    added Skeleton styles fot Themes

--- src/app/shared/cut-description.pipe.ts
+++ src/app/shared/cut-description.pipe.ts
@@ -7,8 +7,8 @@ export class CutDescriptionPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
 
-  	if (value.length > 250) {
-  		return value.substring(0, 250) + '...';
+  	if (value.length > 200) {
+  		return value.substring(0, 200) + '...';
   	} else {
   		return value;
   	}
