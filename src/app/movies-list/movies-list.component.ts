9004486d052ba8f98707ad041c186ddf3092e055
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2017-05-14 08:38:24 +0300

    added Skeleton template for Navbar and Cards List

--- src/app/movies-list/movies-list.component.ts
+++ src/app/movies-list/movies-list.component.ts
@@ -10,20 +10,25 @@ export class MoviesListComponent implements OnInit {
 
 	moviesList: object[] = [];
 	errorMessage: string;
+	skeletonShow: boolean = true;
 
 	constructor(private tmdbService: TMDbService) { }
 
 	ngOnInit() {
+
 		this.getMovies();
+
 	}
 
 	getMovies() {
 		this.tmdbService.getNowPlaying()
 				.subscribe(
 					movies => {
+						this.skeletonShow = false;
 						this.moviesList.push(...movies);
 					},
 					error => {
+						this.skeletonShow = false;
 						this.errorMessage = error;
 						console.error(error);
 					});
